7e8cc9105dc205e95df4a76fdb03d139d4c92058
Jakob Stieger <jakobst20@gmail.com>
2025-09-11 16:21:40 +0200

    Implement counter functionality with increment and decrement buttons

--- coursework/2025-09-11-html-dom-intro/src/index.ts
+++ coursework/2025-09-11-html-dom-intro/src/index.ts
@@ -1,3 +1,22 @@
-const helloWorldHeader = document.getElementById("helloWorldHeader") as HTMLHeadingElement;
+const counterButton = document.getElementById('counterButton') as HTMLButtonElement;
+const counter = document.getElementById('counter') as HTMLParagraphElement;
 
-helloWorldHeader.innerText += "!";
+const decreaseButton = document.getElementById('decreaseButton') as HTMLButtonElement;
+
+let clickCount = 0;
+
+counterButton.addEventListener('click', () => {
+    clickCount++;
+    counter.textContent = `${clickCount}` ;
+        if (clickCount > 0) {
+        counter.style.color = "black"
+    }
+})
+
+decreaseButton.addEventListener('click', () => {
+    clickCount--;
+    counter.textContent = `${clickCount}` ;
+    if (clickCount < 0) {
+        counter.style.color = "red"
+    }
+})
