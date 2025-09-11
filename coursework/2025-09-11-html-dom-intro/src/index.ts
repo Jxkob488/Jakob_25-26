7a775ee50385763a82b46db3b65d0f6d61c52487
Jakob Stieger <jakobst20@gmail.com>
2025-09-11 16:40:59 +0200

    Add increment value selection to counter functionality

--- coursework/2025-09-11-html-dom-intro/src/index.ts
+++ coursework/2025-09-11-html-dom-intro/src/index.ts
@@ -2,11 +2,16 @@ const counterButton = document.getElementById('counterButton') as HTMLButtonElem
 const counter = document.getElementById('counter') as HTMLParagraphElement;
 
 const decreaseButton = document.getElementById('decreaseButton') as HTMLButtonElement;
+const increment = document.getElementById('increment') as HTMLInputElement;
+const select = document.getElementById('incrementValues') as HTMLSelectElement
 
 let clickCount = 0;
+select.addEventListener('change', () => {
+    increment.value = select.value;
+})
 
 counterButton.addEventListener('click', () => {
-    clickCount++;
+    clickCount+= parseInt(increment.value);
     counter.textContent = `${clickCount}` ;
         if (clickCount > 0) {
         counter.style.color = "black"
@@ -14,7 +19,7 @@ counterButton.addEventListener('click', () => {
 })
 
 decreaseButton.addEventListener('click', () => {
-    clickCount--;
+    clickCount-= parseInt(increment.value);
     counter.textContent = `${clickCount}` ;
     if (clickCount < 0) {
         counter.style.color = "red"
